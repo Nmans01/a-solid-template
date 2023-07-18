9d453ba63fc3175c9549da3b76c5a91258dd89e7
Nmans01 <natemans01@gmail.com>
2023-07-18 09:32:14 -0400

    Footer

--- src/modules/common/components/Footer.tsx
+++ src/modules/common/components/Footer.tsx
@@ -2,7 +2,8 @@ import { Component } from "solid-js";
 
 export const Footer: Component<{}> = (props) => {
   
-  return <footer class="bg-gray-800 text-white absolute top-[100%] w-full p-8 px-20 shadow-inner">
-    <span class="font-bold">A Solid Template</span> by <a href="https://github.com/Nmans01" class="underline">nmans01</a>
+  return <footer class="bg-gray-800 text-white absolute top-[100%] w-full p-8 px-20 shadow-inner flex justify-between">
+    <div><span class="font-bold">A Solid Template</span> by <a href="https://github.com/Nmans01" class="underline">nmans01</a></div>
+    <span>ඞ</span>
   </footer>;
 };
